660efffabaf48002baf47188fa54a69dfd26e964
atsoc1993 <atsoc1993@gmail.com>
2025-10-19 21:55:41 -0400

    Made page mobile responsive

--- nil-ns/src/App.tsx
+++ nil-ns/src/App.tsx
@@ -10,17 +10,18 @@ export default function App() {
       <h1 className='text-8xl text-center p-6 font-extrabold'>NIL NS</h1>
       <p className='text-center'>Nillion Name Service</p>
 
-      <div className='bg-slate-800 h-1/2 p-12 flex justify-center items-center'>
-        <div className='w-1/2 h-1/6 text-center shadow-lg rounded-2xl shadow-emerald-400 flex'>
-          <input className='rounded-s-xl text-2xl bg-slate-800 w-3/4 h-full text-center focus:border-emerald- focus:outline-none'
+      <div className='bg-slate-800 h-1/2 w-full flex justify-center items-center'>
+        <div className='w-4/5 h-1/6 text-center shadow-lg rounded-2xl shadow-emerald-400 flex'>
+          <input className='rounded-s-xl text-2xl bg-slate-800 w-3/5 h-full text-center focus:border-emerald- focus:outline-none'
             ref={nilNameInputRef}
             value={nilNameText}
+            spellCheck={false}
             onChange={(e) => setNilNameText(e.currentTarget.value)}
             onFocus={() => setNilNameText('')}
             onBlur={() => { if (!nilNameText.endsWith('.nil') && nilNameText) setNilNameText(prev => prev + '.nil') }}
             placeholder='you.nil'
           ></input>
-          <button className='bg-slate-700 rounded-r-xl w-1/4 h-full text-center duration-200 ease-out hover:bg-slate-500'
+          <button className='bg-slate-700 rounded-r-xl w-2/5 h-full text-center duration-200 ease-out hover:bg-slate-500'
           onClick={() => { 
             if (!nilNameText) {
               return 
