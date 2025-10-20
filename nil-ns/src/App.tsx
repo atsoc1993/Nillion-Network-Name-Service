41aaa4e27445295c84993f6e9f024132c10f22e9
atsoc1993 <atsoc1993@gmail.com>
2025-10-19 21:48:35 -0400

    Added tailwind, base name creation UI, no backend logic or actual NIL network implementations as of yet. Requires NilPay, NilAuth NUC generation, and NilDB entry afterwards

--- nil-ns/src/App.tsx
+++ nil-ns/src/App.tsx
@@ -1,6 +1,38 @@
+import { useRef, useState } from 'react'
+import './App.css'
 export default function App() {
+
+  const [nilNameText, setNilNameText] = useState<string>('')
+  const nilNameInputRef = useRef<HTMLInputElement | null>(null)
+
   return (
-    <>
-    </>
+    <div className="p6 h-full w-full bg-slate-800">
+      <h1 className='text-8xl text-center p-6 font-extrabold'>NIL NS</h1>
+      <p className='text-center'>Nillion Name Service</p>
+
+      <div className='bg-slate-800 h-1/2 p-12 flex justify-center items-center'>
+        <div className='w-1/2 h-1/6 text-center shadow-lg rounded-2xl shadow-emerald-400 flex'>
+          <input className='rounded-s-xl text-2xl bg-slate-800 w-3/4 h-full text-center focus:border-emerald- focus:outline-none'
+            ref={nilNameInputRef}
+            value={nilNameText}
+            onChange={(e) => setNilNameText(e.currentTarget.value)}
+            onFocus={() => setNilNameText('')}
+            onBlur={() => { if (!nilNameText.endsWith('.nil') && nilNameText) setNilNameText(prev => prev + '.nil') }}
+            placeholder='you.nil'
+          ></input>
+          <button className='bg-slate-700 rounded-r-xl w-1/4 h-full text-center duration-200 ease-out hover:bg-slate-500'
+          onClick={() => { 
+            if (!nilNameText) {
+              return 
+            } else {
+              console.log("Calculate & Prompt Payment here, create backend listener to await payment, then create DB entry and display user page afterwards for configuration. The celebration effect would be nice as well.")
+            }
+          }}
+          >
+            Create
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
