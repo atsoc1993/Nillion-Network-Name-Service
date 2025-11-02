37e67252485333bf94d53eb21b17d2c80908859a
atsoc1993 <atsoc1993@gmail.com>
2025-11-02 17:59:01 -0500

    Set up a browser router for new pages, Profile, Name & send for payments, also included a provider for the new Keplr hook

--- nil-ns/src/main.tsx
+++ nil-ns/src/main.tsx
@@ -1,21 +1,35 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Profile from './Profile.tsx'
+import NameDetail from './NameDetail.tsx'
+import SendPayment from './SendPayment.tsx'
+import { KeplrContext } from './WalletContext.tsx'
+import type { Keplr } from '@keplr-wallet/provider-extension'
+import { useState } from 'react'
 
+declare global {
+  interface Window {
+    keplr?: any; 
+  }
+}
 
-// window.onload = async () => {
-//   if (!window.keplr) {
-//     console.log("Keplr doesnt exist")
-//     alert("Please install the Keplr extension.");
-//     return;
-//   } else {
-//     console.log("Kepler exists")
-//   }
+function Root() {
+  const [keplr, setKeplr] = useState<Keplr | undefined>(undefined)
 
-//   const keplr = window.keplr;
+  return (
+    <KeplrContext.Provider value={[keplr, setKeplr]}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} Component={App}></Route>
+          <Route path={'/profile'} Component={Profile}></Route>
+          <Route path={'/name/:id'} Component={NameDetail}></Route>
+          <Route path={'/send'} Component={SendPayment}></Route>
+        </Routes>
+      </BrowserRouter>
+    </KeplrContext.Provider>
+  )
+}
 
-//   const chainId = "cosmoshub-4";
-//   await keplr.enable(chainId);
-// };
-createRoot(document.getElementById('root')!).render(
-  <App />
-)
+createRoot(document.getElementById('root')!).render(<Root />)
+
